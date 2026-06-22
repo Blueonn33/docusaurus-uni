24696efce99aa9d678d5d6dc0bb26060da5d3c6d
Martin Marinov <m.marinov1733@gmail.com>
2026-06-22 11:55:06 +0300

    начална страница

--- unidocs/docusaurus.config.js
+++ unidocs/docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Документация',
+  tagline: 'Съдържа всичко най-важно за предметите Уеб дизайн и Уеб програмиране на .NET',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
