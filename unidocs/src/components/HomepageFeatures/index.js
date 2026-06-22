33dbbb326f11f0d1ae690533c4a014ab0feb4038
Martin Marinov <m.marinov1733@gmail.com>
2026-06-22 11:59:03 +0300

    начална страница

--- unidocs/src/components/HomepageFeatures/index.js
+++ unidocs/src/components/HomepageFeatures/index.js
@@ -4,32 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Лесно използване',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Документацията е написана на разбираем език. Картинките и примерите с код, помагат за по-лесното усвояване на материала.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Фокус върху важното',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Включена е само най-важната информация за темите.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Използване на React',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Документацията редовно ще бъде обновявана чрез използване на React.
       </>
     ),
   },
