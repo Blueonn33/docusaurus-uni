6e3a9c6000ddd1e433b63192457453f32e04e369
Blueonn33 <m.marinov1733@gmail.com>
2026-08-30 16:41:02 +0300

    тъпи примери

--- unidocs/src/components/HtmlPlayground.jsx
+++ unidocs/src/components/HtmlPlayground.jsx
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import CodeBlock from "@theme/CodeBlock";
+
+function Preview({ code }) {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    if (!containerRef.current) return;
+
+    const shadowRoot =
+      containerRef.current.shadowRoot ||
+      containerRef.current.attachShadow({ mode: "open" });
+
+    shadowRoot.innerHTML = code;
+  }, [code]);
+
+  return (
+    <div
+      ref={containerRef}
+      style={{
+        width: "100%",
+        minHeight: "200px",
+      }}
+    />
+  );
+}
 
 export default function HtmlPlayground({ code }) {
   return (
@@ -12,9 +36,17 @@ export default function HtmlPlayground({ code }) {
       }}
     >
       {/* CODE */}
-      <div style={{ flex: "1 1 0", minWidth: 0 }}>
+      <div
+        style={{
+          flex: "1 1 0",
+          minWidth: 0,
+        }}
+      >
         <h3>Code</h3>
-        <CodeBlock language="html">{code}</CodeBlock>
+
+        <CodeBlock language="html">
+          {code}
+        </CodeBlock>
       </div>
 
       {/* PREVIEW */}
@@ -28,8 +60,10 @@ export default function HtmlPlayground({ code }) {
         }}
       >
         <h3>Preview</h3>
-        <div dangerouslySetInnerHTML={{ __html: code }} />
+
+        <Preview code={code} />
       </div>
     </div>
   );
 }
+
